5f3a01c8785b8b0c142f6669bc2a616abcd21c69
GitHub Action <action@github.com>
2019-11-19 11:30:20 +0000

    black code formatting

--- tests/unit/cocoon/test_instruments.py
+++ tests/unit/cocoon/test_instruments.py
@@ -5,44 +5,42 @@ from parameterized import parameterized
 
 
 class CocoonUtilitiesTests(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls) -> None:
         cls.logger = logger.LusidLogger("debug")
 
-    @parameterized.expand([
+    @parameterized.expand(
         [
-            "Standard full key",
-            "Instrument/default/Figi",
-            True,
-            "Instrument/default/Figi"
-        ],
-        [
-            "Full key specified as short key",
-            "Figi",
-            True,
-            "Instrument/default/Figi"
-        ],
-        [
-            "Full key specified as property",
-            "Instrument/PB/Isin",
-            True,
-            "Instrument/PB/Isin"
-        ],
-        [
-            "Standard short key",
-            "Isin",
-            False,
-            "Isin"
-        ],
-        [
-            "Short key specifed as long key",
-            "Instrument/default/Isin",
-            False,
-            "Isin"
+            [
+                "Standard full key",
+                "Instrument/default/Figi",
+                True,
+                "Instrument/default/Figi",
+            ],
+            [
+                "Full key specified as short key",
+                "Figi",
+                True,
+                "Instrument/default/Figi",
+            ],
+            [
+                "Full key specified as property",
+                "Instrument/PB/Isin",
+                True,
+                "Instrument/PB/Isin",
+            ],
+            ["Standard short key", "Isin", False, "Isin"],
+            [
+                "Short key specifed as long key",
+                "Instrument/default/Isin",
+                False,
+                "Isin",
+            ],
         ]
-    ])
-    def test_create_identifiers_prepare_key(self, _, identifier_lusid, full_key_format, expected_outcome) -> None:
+    )
+    def test_create_identifiers_prepare_key(
+        self, _, identifier_lusid, full_key_format, expected_outcome
+    ) -> None:
         """
         Tests that key preparation for identifiers works as expected
 
@@ -55,13 +53,9 @@ class CocoonUtilitiesTests(unittest.TestCase):
         """
 
         output_key = prepare_key(
-            identifier_lusid=identifier_lusid,
-            full_key_format=full_key_format)
-
-
-        print (output_key, expected_outcome)
-
-        self.assertEqual(
-            output_key,
-            expected_outcome
+            identifier_lusid=identifier_lusid, full_key_format=full_key_format
         )
+
+        print(output_key, expected_outcome)
+
+        self.assertEqual(output_key, expected_outcome)
