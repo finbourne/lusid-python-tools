6e1368c382513fc04054e97c9f17882cc9b0e18c
Michael McGarry <mike.mcgarry@finbourne.com>
2019-11-26 09:40:08 +0000

    DEV-5912 Added a deployment script for Travis to deploy to PyPi

--- publish.sh
+++ publish.sh
@@ -2,31 +2,20 @@
 
 if [[ (${#1} -eq 0) ]] ; then
     echo
-    echo "[ERROR] missing PyPi repo"
-    echo
-    exit 1
-fi
-
-
-if [[ (${#2} -eq 0) ]] ; then
-    echo
     echo "[ERROR] missing username"
     echo
     exit 1
 fi
 
-if [[ (${#3} -eq 0) ]] ; then
+if [[ (${#2} -eq 0) ]] ; then
     echo
     echo "[ERROR] missing password"
     echo
     exit 1
 fi
 
-pypi_repo=$1
-pypi_username=$2
-pypi_password=$3
-
-cd ..
+pypi_username=$1
+pypi_password=$2
 
 api_version=$(cat __version__.py | grep __version__ |  awk '{split($0, a, "="); print a[2]}' | tr -d ' "')
 
@@ -36,7 +25,3 @@ python setup.py sdist bdist_wheel
 
 # upload
 twine upload -u $pypi_username -p $pypi_password dist/*
-
-cd ..
-
-rm -f publish.sh
