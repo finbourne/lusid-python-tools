5ffa8a1b2473aeeca8c5471b400e6b7d6b0673da
stephen.lemasney@finbourne.com <stephen.lemasney@finbourne.com>
2020-01-02 11:39:36 +0000

    adding pandas utils

---
+++ lusidtools/pandas_utils/lusid_pandas.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from flatten_json import flatten
+
+
+def lusid_response_to_data_frame(lusid_response):
+
+    """
+    Description:
+    This function takes a LUSID API response and attempts to convert the response into a Pandas DataFrame or Series.
+    Not all LUSID API responses are the same datatype. Therefore we need to implement some if/else conditional logic to
+    see how the response should be converted.
+
+    In terms of an implementation, the function checks for two attributes on the response:
+
+    (1) A "values" attribute - these are lists of LUSID objects. Example: the "Get Holdings" response has a values
+    attribute which contains a list of individual holding objects.
+
+    (2) A "to_dict" attribute - this is one dictionary of values.
+
+    Parameters:
+    :param lusid_response: a response from the LUSID APIs (e.g. VersionedResourceListOfPortfolioHolding)
+
+    Returns:
+    pandas DataFrame
+
+    """
+
+    if hasattr(lusid_response, "values") and type(lusid_response.values) == list:
+
+        response_df = pd.DataFrame(
+            flatten(value.to_dict(), ".") for value in lusid_response.values
+        )
+
+    elif hasattr(lusid_response, "to_dict"):
+
+        response_df = pd.DataFrame.from_dict((flatten(lusid_response.to_dict(), ".")),
+                                             orient="index",
+                                             columns=["response_values"])
+    else:
+
+        raise TypeError(
+            """Cannot map response object to pandas DataFrame or Series. The LUSID response object must be
+                        either values or to_dict"""
+        )
+
+    return response_df
---
+++ tests/unit/pandas_utils/test_lusid_pandas.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+import lusid
+import lusid.models as models
+import unittest
+from lusidtools.pandas_utils.lusid_pandas import lusid_response_to_data_frame
+import pandas as pd
+
+class TestResponseToPandasObject(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        secrets_file = Path(__file__).parent.parent.parent.joinpath("secrets.json")
+        cls.api_factory = lusid.utilities.ApiClientFactory(
+            api_secrets_filename=secrets_file
+        )
+
+    def test_response_to_df(self):
+
+        holding_1 = lusid.models.portfolio_holding.PortfolioHolding(
+            cost={"amount": 549997.05, "currency": "GBP"},
+            cost_portfolio_ccy={"amount": 0.0, "currency": "GBP"},
+            holding_type="P",
+            instrument_uid="LUID_XQ6VSO8F",
+            properties={},
+            settled_units=137088.0,
+            sub_holding_keys={},
+            transaction=None,
+            units=137088.0,
+        )
+
+        holding_2 = lusid.models.portfolio_holding.PortfolioHolding(
+            cost={"amount": 12345.05, "currency": "GBP"},
+            cost_portfolio_ccy={"amount": 0.0, "currency": "GBP"},
+            holding_type="P",
+            instrument_uid="LUID_123",
+            properties={},
+            settled_units=1372222.0,
+            sub_holding_keys={},
+            transaction=None,
+            units=1372228.0,
+        )
+
+        test_holdings_response = lusid.models.versioned_resource_list_of_portfolio_holding.VersionedResourceListOfPortfolioHolding(
+            version=1, values=[holding_1, holding_2]
+        )
+
+        holdings_df = lusid_response_to_data_frame(test_holdings_response)
+        self.assertIsInstance(holdings_df, pd.DataFrame)
+        self.assertEqual(holdings_df.loc[0]["instrument_uid"], "LUID_XQ6VSO8F")
+
+    def test_instrument_response_to_df(self):
+
+        test_instrument_response = lusid.models.instrument.Instrument(
+            lusid_instrument_id="LUID_TEST",
+            version=1,
+            name="Test LUID",
+            identifiers={},
+            state="Active",
+        )
+
+        instrument_df = lusid_response_to_data_frame(test_instrument_response)
+        self.assertIsInstance(instrument_df, pd.DataFrame)
+        self.assertEqual(instrument_df.loc["lusid_instrument_id"][0], "LUID_TEST")
+
+    def test_transaction_alias_response_to_df(self):
+
+        transaction_alias_response = lusid.models.transaction_configuration_type_alias.TransactionConfigurationTypeAlias(
+            type="Buy",
+            description="Purchase",
+            transaction_class="Basic",
+            transaction_group="Default",
+            transaction_roles="LongLonger",
+        )
+
+        transaction_alias_df = lusid_response_to_data_frame(transaction_alias_response)
+        self.assertIsInstance(transaction_alias_df, pd.DataFrame)
+        self.assertEqual(transaction_alias_df.loc["transaction_class"][0], "Basic")
+
+    def test_response_to_df_fail(self):
+
+        self.assertRaises(TypeError, lambda: lusid_response_to_data_frame("test_string"))
+
+    def test_response_none(self):
+
+        self.assertRaises(TypeError, lambda: lusid_response_to_data_frame(None))
