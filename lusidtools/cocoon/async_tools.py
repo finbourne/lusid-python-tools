cb7fc4723449b2bd0899cdc5c4c5f0f7e8d30bca
Michael McGarry <mike.mcgarry@finbourne.com>
2020-01-02 17:03:26 +0000

    DEV Force publish of 0.1.1a47

--- lusidtools/cocoon/async_tools.py
+++ lusidtools/cocoon/async_tools.py
@@ -34,6 +34,8 @@ def run_in_executor(f):
     @functools.wraps(f)
     def inner(*args, **kwargs):
         loop = asyncio.get_running_loop()
-        return loop.run_in_executor(None, lambda: f(*args, **kwargs))
+        
+        return loop.run_in_executor(None, lambda: f(
+            *args, **kwargs))
 
     return inner
