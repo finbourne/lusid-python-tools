3df253c70254c6d66a385fd894349549ffcba28f
Michael McGarry <mike.mcgarry@finbourne.com>
2020-01-03 10:55:41 +0000

    Removed reference to GBp

--- lusidtools/cocoon/async_tools.py
+++ lusidtools/cocoon/async_tools.py
@@ -34,7 +34,7 @@ def run_in_executor(f):
     @functools.wraps(f)
     def inner(*args, **kwargs):
         loop = asyncio.get_running_loop()
-        
+
         return loop.run_in_executor(None, lambda: f(
             *args, **kwargs))
 
