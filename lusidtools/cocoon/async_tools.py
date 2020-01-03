8b99b1f116c7651a41e914ab617109e137bdbe1b
GitHub Action <action@github.com>
2020-01-03 11:07:54 +0000

    black code formatting

--- lusidtools/cocoon/async_tools.py
+++ lusidtools/cocoon/async_tools.py
@@ -35,7 +35,6 @@ def run_in_executor(f):
     def inner(*args, **kwargs):
         loop = asyncio.get_running_loop()
 
-        return loop.run_in_executor(None, lambda: f(
-            *args, **kwargs))
+        return loop.run_in_executor(None, lambda: f(*args, **kwargs))
 
     return inner
