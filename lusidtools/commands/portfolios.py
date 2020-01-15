33c10a0b1eaa1bd1cf1a3e6a28ab9d396a819fc7
GitHub Action <action@github.com>
2020-01-15 11:40:45 +0000

    black code formatting

--- lusidtools/commands/portfolios.py
+++ lusidtools/commands/portfolios.py
@@ -7,5 +7,5 @@ def main():
     qry.main()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- lusidtools/commands/txn_config.py
+++ lusidtools/commands/txn_config.py
@@ -5,5 +5,5 @@ def main():
     cfg.main()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
