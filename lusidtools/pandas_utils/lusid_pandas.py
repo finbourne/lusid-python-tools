0d0a6ed5eb1d009de7b4462998903bbfdd295c02
Stephen LM <stephen.lemasney@finbourne.com>
2020-02-06 11:33:21 +0000

    DEV-6664 updating formatter (thanks WS!)

--- lusidtools/pandas_utils/lusid_pandas.py
+++ lusidtools/pandas_utils/lusid_pandas.py
@@ -2,8 +2,7 @@ import pandas as pd
 from flatten_json import flatten
 
 
-def lusid_response_to_data_frame(lusid_response):
-
+def lusid_response_to_data_frame(lusid_response, rename_properties: bool = False):
     """
     Description:
     This function takes a LUSID API response and attempts to convert the response into a Pandas DataFrame or Series.
@@ -19,6 +18,8 @@ def lusid_response_to_data_frame(lusid_response):
 
     Parameters:
     :param lusid_response: a response from the LUSID APIs (e.g. VersionedResourceListOfPortfolioHolding)
+    :param bool format_df: this parameter formats the returned DataFrame. Specifically, the formatter does two things;
+    (1) removes any metadata columns for properties and SHKs, and (2) simplifies the naming of column headers for properties and SHKs.
 
     Returns:
     pandas DataFrame
@@ -45,4 +46,43 @@ def lusid_response_to_data_frame(lusid_response):
                         either values or to_dict"""
         )
 
-    return response_df
+    if rename_properties:
+
+        # Collect columns to drop - these are meta-data columns for properties and sub-holding keys
+        columns_to_drop = list(
+            response_df.filter(
+                regex="(^sub_holding_keys|^properties).*(key$|metric_value$|effective_from$)"
+            ).columns
+        )
+
+        response_df.drop(columns_to_drop, axis=1, inplace=True)
+
+        # Rename the properties and sub-holding keys column to show the property "code" only
+        # Recall the format is "domain/scope/code"
+
+        rename_dict = {}
+
+        columns_to_rename = response_df.filter(
+            regex="(^sub_holding_keys|^properties).*(label_value$|value.metric_value.value$)"
+        ).columns
+
+        for column in columns_to_rename:
+
+            # find the property code substring
+            # The property code should always be displayed after the second "/"
+            # Example: sub_holding_keys.Transaction/MultiAssetScope/PortionSubClass.value.label_value
+            property_code = str(column[(column.rfind("/") + 1):])[:column[(column.rfind("/") + 1):].find(".")]
+
+            # find the property scope substring
+            scope_string = column[(column.find("/") + 1): (column.rfind("/"))]
+
+            # find the property or SHK suffix
+            p_or_shk = column[:column.find(".")].title().replace("_", "")
+
+            rename_dict[column] = (
+                    property_code + "(" + scope_string + "-" + p_or_shk + ")"
+            )
+
+        response_df.rename(columns=rename_dict, inplace=True)
+
+    return response_df.dropna(axis=1, how="all")
--- tests/unit/pandas_utils/test_lusid_pandas.py
+++ tests/unit/pandas_utils/test_lusid_pandas.py
@@ -4,6 +4,7 @@ import lusid.models as models
 import unittest
 from lusidtools.pandas_utils.lusid_pandas import lusid_response_to_data_frame
 import pandas as pd
+import datetime
 
 
 class TestResponseToPandasObject(unittest.TestCase):
@@ -73,8 +74,12 @@ class TestResponseToPandasObject(unittest.TestCase):
         )
 
         transaction_alias_df = lusid_response_to_data_frame(transaction_alias_response)
+        transaction_alias_df_format = lusid_response_to_data_frame(transaction_alias_response, rename_properties=True)
+
         self.assertIsInstance(transaction_alias_df, pd.DataFrame)
         self.assertEqual(transaction_alias_df.loc["transaction_class"][0], "Basic")
+        self.assertIsInstance(transaction_alias_df_format, pd.DataFrame)
+        self.assertEqual(transaction_alias_df_format.loc["transaction_class"][0], "Basic")
 
     def test_response_to_df_fail(self):
 
@@ -85,3 +90,66 @@ class TestResponseToPandasObject(unittest.TestCase):
     def test_response_none(self):
 
         self.assertRaises(TypeError, lambda: lusid_response_to_data_frame(None))
+
+    def test_response_to_df_format(self):
+
+        holding_1 = lusid.models.portfolio_holding.PortfolioHolding(
+            cost={"amount": 12345.05, "currency": "GBP"},
+            cost_portfolio_ccy={"amount": 0.0, "currency": "GBP"},
+            holding_type="P",
+            instrument_uid="LUID_123",
+            properties={
+                "Instrument/MultiAssetScope/shares_out": {
+                    "effective_from": datetime.datetime(1, 1, 1, 0, 0),
+                    "key": "Instrument/MultiAssetScope/shares_out",
+                    "value": {
+                        "label_value": None,
+                        "metric_value": {"unit": None, "value": 100000.0},
+                    },
+                },
+                "Instrument/MultiAssetScope/sm_instrument_type": {
+                    "effective_from": datetime.datetime(1, 1, 1, 0, 0),
+                    "key": "Instrument/MultiAssetScope/sm_instrument_type",
+                    "value": {"label_value": "Equity", "metric_value": None},
+                },
+            },
+            settled_units=1372222.0,
+            sub_holding_keys={
+                "Transaction/MultiAssetScope/PortionClass": {
+                    "key": "Transaction/MultiAssetScope/PortionClass",
+                    "value": {
+                        "label_value": "Absolute " "Return " "(other)",
+                        "metric_value": None,
+                    },
+                },
+                "Transaction/MultiAssetScope/PortionRegion": {
+                    "key": "Transaction/MultiAssetScope/PortionRegion",
+                    "value": {"label_value": "None", "metric_value": None},
+                },
+                "Transaction/MultiAssetScope/PortionSubClass": {
+                    "key": "Transaction/MultiAssetScope/PortionSubClass",
+                    "value": {
+                        "label_value": "Absolute " "Return " "(other)",
+                        "metric_value": None,
+                    },
+                },
+            },
+            transaction=None,
+            units=1372228.0,
+        )
+
+        test_holdings_response = lusid.models.versioned_resource_list_of_portfolio_holding.VersionedResourceListOfPortfolioHolding(
+            version=1, values=[holding_1]
+        )
+
+        holdings_df = lusid_response_to_data_frame(
+            test_holdings_response, rename_properties=True
+        )
+
+        df_columns = holdings_df.columns
+
+        self.assertIsInstance(holdings_df, pd.DataFrame)
+        self.assertEqual(holdings_df.loc[0]["instrument_uid"], "LUID_123")
+        self.assertIn("PortionClass(MultiAssetScope-SubHoldingKeys)", df_columns)
+        self.assertIn("shares_out(MultiAssetScope-Properties)", df_columns)
+        self.assertNotIn("JunkBadColumn(SubHoldingKey)", df_columns)
