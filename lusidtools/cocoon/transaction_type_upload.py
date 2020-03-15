f37755a7b45698020634d0e7c6af3eacea5e8307
Stephen LM <stephen.lemasney@finbourne.com>
2020-03-15 12:53:35 +0000

    adding transaction type upload

--- lusidtools/cocoon/transaction_type_upload.py
+++ lusidtools/cocoon/transaction_type_upload.py
@@ -2,40 +2,46 @@ import lusid
 import lusid.models as models
 import logging
 
+logger = logging.getLogger()
+
+
 def create_transaction_type_configuration(api_factory, new_alias, movements):
     """
     This function creates a transaction type configuration if it doesn't already exist.
 
-    param (lusid.utilities.ClientApiFactory) api_factory: The LUSID api factory to use
-    param (lusid.models.TransactionConfigurationTypeAlias) new_alias: A new alias object with a type and group.
-    param (list[lusid.models.TransactionConfigurationMovementDataRequest]) movements: The movements to use for the transaction type.
+    :param lusid.utilities.ClientApiFactory api_factory: The LUSID api factory to use
+    :param lusid.models.TransactionConfigurationTypeAlias new_alias: An aliases with type and group
+    :param list[lusid.models.TransactionConfigurationMovementDataRequest] movements: The movements to use for
+    transaction type
 
     return (lusid.models.createtransactiontyperesponse) response: The response from creating the transaction type
     """
+
+
 
     # Call LUSID to get your transaction type configuration
     response = api_factory.build(
         lusid.api.SystemConfigurationApi
     ).list_configuration_transaction_types()
 
-    current_aliases = [
+    aliases_current = [
         (alias.type, alias.transaction_group)
         for transaction_grouping in response.transaction_configs
         for alias in transaction_grouping.aliases
     ]
 
-    logging.info(
-        f"The LUSID enviornment currently has {len(current_aliases)} transaction aliases"
+    logger.info(
+        f"The LUSID enviornment currently has {len(aliases_current)} transaction aliases"
     )
 
-    if (new_alias.type, new_alias.transaction_group) in current_aliases:
+    if (new_alias.type, new_alias.transaction_group) in aliases_current:
         logging.info(
             "The following alias already exists: "
             + f"Type of {new_alias.type} with source {new_alias.transaction_group}"
         )
         return response
 
-    logging.info(f"Creating a new transaction aliases called: {alias_new.type}")
+    logger.info(f"Creating a new transaction aliases called: {new_alias.type}")
 
     response = api_factory.build(
         lusid.api.SystemConfigurationApi
--- tests/integration/cocoon/test_create_transaction_type.py
+++ tests/integration/cocoon/test_create_transaction_type.py
@@ -1,32 +1,90 @@
 import unittest
 from pathlib import Path
-import pandas as pd
-import json
+
 import lusid
 import lusid.models as models
 from lusidtools.cocoon.utilities import create_scope_id
+from lusidtools.cocoon.transaction_type_upload import (
+    create_transaction_type_configuration,
+)
 import logging
 
 logger = logging.getLogger()
 
 
-class CocoonTestPortfolioGroup(unittest.TestCase):
+class CocoonTestTransactionTypeUpload(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
 
-        cls.portfolio_scope = create_scope_id()
         secrets_file = Path(__file__).parent.parent.parent.joinpath("secrets.json")
         cls.api_factory = lusid.utilities.ApiClientFactory(
             api_secrets_filename=secrets_file
         )
 
-    def log_error_requests_title(cls, domain, responses):
-        if len(responses.get(domain, {}).get("errors", [])) > 0:
-            for error in responses[domain]["errors"]:
-                return logger.error(json.loads(error.body)["title"])
+        cls.new_alias = models.TransactionConfigurationTypeAlias(
+            type=create_scope_id().replace("-", ""),
+            description="TESTBUY1",
+            transaction_class="TESTBUY1",
+            transaction_group="SYSTEM1",
+            transaction_roles="AllRoles",
+        )
 
+        cls.movements = [
+            models.TransactionConfigurationMovementDataRequest(
+                movement_types="StockMovement",
+                side="Side1",
+                direction=1,
+                properties=None,
+                mappings=None,
+            ),
+            models.TransactionConfigurationMovementDataRequest(
+                movement_types="CashCommitment",
+                side="Side2",
+                direction=-1,
+                properties=None,
+                mappings=None,
+            ),
+        ]
 
-    def csv_to_data_frame_with_scope(cls, csv, scope):
-        data_frame = pd.read_csv(Path(__file__).parent.joinpath(csv))
-        data_frame["Scope"] = scope
-        return data_frame
+        cls.response = create_transaction_type_configuration(
+            cls.api_factory, cls.new_alias, cls.movements
+        )
+
+    def test_create_new_txn_type(self):
+
+        """
+        Create a new transaction type.
+        Verify that the new transaction type is created.
+        """
+
+        self.assertEqual(
+            set(
+                sorted(
+                    self.response.transaction_configs[-1].aliases[0].to_dict().items()
+                )
+            ),
+            set(self.new_alias.to_dict().items()),
+        )
+
+    def test_create_current_txn_type(self):
+
+        """
+        Attempt to create a transaction type which already exists.
+        Verify that function returns the correct log message.
+        """
+
+        with self.assertLogs() as context_manager:
+
+            create_transaction_type_configuration(
+                self.api_factory, self.new_alias, self.movements
+            )
+
+            print(context_manager.output)
+            print(self.new_alias.type)
+
+            self.assertEqual(
+                f"INFO:root:The following alias already exists: Type of {self.new_alias.type} with source {self.new_alias.transaction_group}",
+                context_manager.output[1],
+            )
+
+            print("logs: ", context_manager.output)
