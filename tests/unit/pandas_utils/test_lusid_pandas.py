311a854d74a1ee2e7f79e497db926d027741bc03
Stephen LM <20342969+slemasne@users.noreply.github.com>
2020-03-19 15:57:07 +0000

    adding rename mappings (#67)
    Co-authored-by: Stephen LM <slemasne@github-no-reply.com>

--- tests/unit/pandas_utils/test_lusid_pandas.py
+++ tests/unit/pandas_utils/test_lusid_pandas.py
@@ -15,9 +15,7 @@ class TestResponseToPandasObject(unittest.TestCase):
             api_secrets_filename=secrets_file
         )
 
-    def test_response_to_df(self):
-
-        holding_1 = lusid.models.portfolio_holding.PortfolioHolding(
+        cls.holding_no_props_1 = lusid.models.portfolio_holding.PortfolioHolding(
             cost={"amount": 549997.05, "currency": "GBP"},
             cost_portfolio_ccy={"amount": 0.0, "currency": "GBP"},
             holding_type="P",
@@ -29,7 +27,7 @@ class TestResponseToPandasObject(unittest.TestCase):
             units=137088.0,
         )
 
-        holding_2 = lusid.models.portfolio_holding.PortfolioHolding(
+        cls.holding_no_props_2 = lusid.models.portfolio_holding.PortfolioHolding(
             cost={"amount": 12345.05, "currency": "GBP"},
             cost_portfolio_ccy={"amount": 0.0, "currency": "GBP"},
             holding_type="P",
@@ -41,63 +39,7 @@ class TestResponseToPandasObject(unittest.TestCase):
             units=1372228.0,
         )
 
-        test_holdings_response = lusid.models.versioned_resource_list_of_portfolio_holding.VersionedResourceListOfPortfolioHolding(
-            version=1, values=[holding_1, holding_2]
-        )
-
-        holdings_df = lusid_response_to_data_frame(test_holdings_response)
-        self.assertIsInstance(holdings_df, pd.DataFrame)
-        self.assertEqual(holdings_df.loc[0]["instrument_uid"], "LUID_XQ6VSO8F")
-
-    def test_instrument_response_to_df(self):
-
-        test_instrument_response = lusid.models.instrument.Instrument(
-            lusid_instrument_id="LUID_TEST",
-            version=1,
-            name="Test LUID",
-            identifiers={},
-            state="Active",
-        )
-
-        instrument_df = lusid_response_to_data_frame(test_instrument_response)
-        self.assertIsInstance(instrument_df, pd.DataFrame)
-        self.assertEqual(instrument_df.loc["lusid_instrument_id"][0], "LUID_TEST")
-
-    def test_transaction_alias_response_to_df(self):
-
-        transaction_alias_response = lusid.models.transaction_configuration_type_alias.TransactionConfigurationTypeAlias(
-            type="Buy",
-            description="Purchase",
-            transaction_class="Basic",
-            transaction_group="Default",
-            transaction_roles="LongLonger",
-        )
-
-        transaction_alias_df = lusid_response_to_data_frame(transaction_alias_response)
-        transaction_alias_df_format = lusid_response_to_data_frame(
-            transaction_alias_response, rename_properties=True
-        )
-
-        self.assertIsInstance(transaction_alias_df, pd.DataFrame)
-        self.assertEqual(transaction_alias_df.loc["transaction_class"][0], "Basic")
-        self.assertIsInstance(transaction_alias_df_format, pd.DataFrame)
-        self.assertEqual(
-            transaction_alias_df_format.loc["transaction_class"][0], "Basic"
-        )
-
-    def test_response_to_df_fail(self):
-
-        self.assertRaises(
-            TypeError, lambda: lusid_response_to_data_frame("test_string")
-        )
-
-    def test_response_none(self):
-
-        self.assertRaises(TypeError, lambda: lusid_response_to_data_frame(None))
-
-    def test_response_to_df_format(self):
-
-        holding_1 = lusid.models.portfolio_holding.PortfolioHolding(
+        cls.holding_1 = lusid.models.portfolio_holding.PortfolioHolding(
             cost={"amount": 12345.05, "currency": "GBP"},
             cost_portfolio_ccy={"amount": 0.0, "currency": "GBP"},
             holding_type="P",
@@ -142,8 +84,66 @@ class TestResponseToPandasObject(unittest.TestCase):
             units=1372228.0,
         )
 
+    def test_response_to_df(self):
+
         test_holdings_response = lusid.models.versioned_resource_list_of_portfolio_holding.VersionedResourceListOfPortfolioHolding(
-            version=1, values=[holding_1]
+            version=1, values=[self.holding_no_props_1, self.holding_no_props_2]
+        )
+
+        holdings_df = lusid_response_to_data_frame(test_holdings_response)
+        self.assertEqual(type(holdings_df), pd.DataFrame)
+        self.assertEqual(holdings_df.loc[0]["instrument_uid"], "LUID_XQ6VSO8F")
+
+    def test_instrument_response_to_df(self):
+
+        test_instrument_response = lusid.models.instrument.Instrument(
+            lusid_instrument_id="LUID_TEST",
+            version=1,
+            name="Test LUID",
+            identifiers={},
+            state="Active",
+        )
+
+        instrument_df = lusid_response_to_data_frame(test_instrument_response)
+        self.assertEqual(type(instrument_df), pd.DataFrame)
+        self.assertEqual(instrument_df.loc["lusid_instrument_id"][0], "LUID_TEST")
+
+    def test_transaction_alias_response_to_df(self):
+
+        transaction_alias_response = lusid.models.transaction_configuration_type_alias.TransactionConfigurationTypeAlias(
+            type="Buy",
+            description="Purchase",
+            transaction_class="Basic",
+            transaction_group="Default",
+            transaction_roles="LongLonger",
+        )
+
+        transaction_alias_df = lusid_response_to_data_frame(transaction_alias_response)
+        transaction_alias_df_format = lusid_response_to_data_frame(
+            transaction_alias_response, rename_properties=True
+        )
+
+        self.assertEqual(type(transaction_alias_df), pd.DataFrame)
+        self.assertEqual(transaction_alias_df.loc["transaction_class"][0], "Basic")
+        self.assertEqual(type(transaction_alias_df_format), pd.DataFrame)
+        self.assertEqual(
+            transaction_alias_df_format.loc["transaction_class"][0], "Basic"
+        )
+
+    def test_response_to_df_fail(self):
+
+        self.assertRaises(
+            TypeError, lambda: lusid_response_to_data_frame("test_string")
+        )
+
+    def test_response_none(self):
+
+        self.assertRaises(TypeError, lambda: lusid_response_to_data_frame(None))
+
+    def test_response_to_df_format(self):
+
+        test_holdings_response = lusid.models.versioned_resource_list_of_portfolio_holding.VersionedResourceListOfPortfolioHolding(
+            version=1, values=[self.holding_1]
         )
 
         holdings_df = lusid_response_to_data_frame(
@@ -152,8 +152,136 @@ class TestResponseToPandasObject(unittest.TestCase):
 
         df_columns = holdings_df.columns
 
-        self.assertIsInstance(holdings_df, pd.DataFrame)
+        self.assertEqual(type(holdings_df), pd.DataFrame)
         self.assertEqual(holdings_df.loc[0]["instrument_uid"], "LUID_123")
         self.assertIn("PortionClass(MultiAssetScope-SubHoldingKeys)", df_columns)
         self.assertIn("shares_out(MultiAssetScope-Properties)", df_columns)
         self.assertNotIn("JunkBadColumn(SubHoldingKey)", df_columns)
+
+    def test_response_to_df_rename_mapping_format(self):
+
+        rename_mapping = {
+            "instrument_uid": "Instrument ID",
+            "holding_type": "Holding Type",
+            "units": "Units",
+            "settled_units": "Settled Units",
+            "cost.amount": "Quantity",
+            "cost.currency": "Cost Currency",
+        }
+
+        test_holdings_response = lusid.models.versioned_resource_list_of_portfolio_holding.VersionedResourceListOfPortfolioHolding(
+            version=1, values=[self.holding_1]
+        )
+
+        holdings_df = lusid_response_to_data_frame(
+            test_holdings_response,
+            rename_properties=True,
+            column_name_mapping=rename_mapping,
+        )
+
+        df_columns = holdings_df.columns
+
+        self.assertEqual(type(holdings_df), pd.DataFrame)
+        self.assertEqual(holdings_df.loc[0]["Instrument ID"], "LUID_123")
+        self.assertIn("PortionClass(MultiAssetScope-SubHoldingKeys)", df_columns)
+        self.assertTrue(
+            set(
+                ["Holding Type", "Units", "Settled Units", "Quantity", "Cost Currency"]
+            ).issubset(set(df_columns))
+        )
+
+    def test_response_to_df_rename_mapping_no_format(self):
+
+        rename_mapping = {
+            "instrument_uid": "Instrument ID",
+            "holding_type": "Holding Type",
+            "units": "Units",
+            "settled_units": "Settled Units",
+            "cost.amount": "Quantity",
+            "cost.currency": "Cost Currency",
+        }
+
+        test_holdings_response = lusid.models.versioned_resource_list_of_portfolio_holding.VersionedResourceListOfPortfolioHolding(
+            version=1, values=[self.holding_1]
+        )
+
+        holdings_df = lusid_response_to_data_frame(
+            test_holdings_response,
+            rename_properties=False,
+            column_name_mapping=rename_mapping,
+        )
+
+        df_columns = holdings_df.columns
+
+        self.assertEqual(type(holdings_df), pd.DataFrame)
+        self.assertEqual(holdings_df.loc[0]["Instrument ID"], "LUID_123")
+        self.assertEqual(set(["Holding Type", "Units", "Settled Units", "Quantity", "Cost Currency"]),
+            set(
+                ["Holding Type", "Units", "Settled Units", "Quantity", "Cost Currency"]
+            ).intersection(set(df_columns))
+        )
+
+    def test_response_to_df_rename_mapping_with_column_not_exist(self):
+
+        rename_mapping = {
+            "instrument_uid": "Instrument ID",
+            "holding_type": "Holding Type",
+            "test_not_exist": "Units",
+            "settled_units": "Settled Units",
+            "cost.amount": "Quantity",
+            "cost.currency": "Cost Currency",
+        }
+
+        test_holdings_response = lusid.models.versioned_resource_list_of_portfolio_holding.VersionedResourceListOfPortfolioHolding(
+            version=1, values=[self.holding_1]
+        )
+
+        holdings_df = lusid_response_to_data_frame(
+            test_holdings_response,
+            rename_properties=False,
+            column_name_mapping=rename_mapping,
+        )
+
+        df_columns = holdings_df.columns
+
+        self.assertEqual(type(holdings_df), pd.DataFrame)
+        self.assertEqual(holdings_df.loc[0]["Instrument ID"], "LUID_123")
+        self.assertEqual(set(["Holding Type", "Quantity", "Cost Currency"]),
+            set(
+                ["Holding Type", "Quantity", "Cost Currency"]
+            ).intersection(set(df_columns))
+        )
+        self.assertNotIn("settled_units", df_columns)
+
+    def test_response_to_df_rename_property(self):
+
+        rename_mapping = {
+            "instrument_uid": "Instrument ID",
+            "holding_type": "Holding Type",
+            "units": "Units",
+            "settled_units": "Settled Units",
+            "cost.amount": "Quantity",
+            "cost.currency": "Cost Currency",
+            "properties.Instrument/MultiAssetScope/shares_out.key": "Shares Outstanding"
+        }
+
+        test_holdings_response = lusid.models.versioned_resource_list_of_portfolio_holding.VersionedResourceListOfPortfolioHolding(
+            version=1, values=[self.holding_1]
+        )
+
+        holdings_df = lusid_response_to_data_frame(
+            test_holdings_response,
+            rename_properties=False,
+            column_name_mapping=rename_mapping,
+        )
+
+        df_columns = holdings_df.columns
+
+        self.assertEqual(type(holdings_df), pd.DataFrame)
+        self.assertEqual(holdings_df.loc[0]["Instrument ID"], "LUID_123")
+        self.assertEqual(set(["Holding Type", "Units", "Quantity", "Cost Currency", "Shares Outstanding"]),
+            set(
+                ["Holding Type", "Units", "Quantity", "Cost Currency", "Shares Outstanding"]
+            ).intersection(set(df_columns))
+        )
+
