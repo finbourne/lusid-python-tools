ddcdf34e6f4be5e1585c26536503e2cbef4bb10c
Stephen LM <stephen.lemasney@finbourne.com>
2020-04-20 09:03:40 +0100

    pandas utils: handle empty lists

--- lusidtools/pandas_utils/lusid_pandas.py
+++ lusidtools/pandas_utils/lusid_pandas.py
@@ -40,17 +40,22 @@ def lusid_response_to_data_frame(
 
     if type(lusid_response) == list:
 
-        first_item_type = type(lusid_response[0])
+        if len(lusid_response) == 0:
+            return pd.DataFrame()
 
-        if not all(isinstance(x, first_item_type) for x in lusid_response):
-            raise TypeError("All items in list must be of same data type")
+        else:
 
-        if not hasattr(first_item_type, "to_dict"):
-            raise TypeError("All object items in list must have a to_dict() method")
+            first_item_type = type(lusid_response[0])
 
-        response_df = pd.DataFrame(
-            flatten(value.to_dict(), ".") for value in lusid_response
-        )
+            if not all(isinstance(x, first_item_type) for x in lusid_response):
+                raise TypeError("All items in list must be of same data type")
+
+            if not hasattr(first_item_type, "to_dict"):
+                raise TypeError("All object items in list must have a to_dict() method")
+
+            response_df = pd.DataFrame(
+                flatten(value.to_dict(), ".") for value in lusid_response
+            )
 
     # Check if lusid_response has a values attribute with data type of list
 
