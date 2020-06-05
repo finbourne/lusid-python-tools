d6e27deac3cbed8cde0c57dacc52bb006acd5a41
Stephen LeMasney <stephen.lemasney@finbourne.com>
2020-06-05 16:18:57 +0100

    bump version test

--- ci_tools/bumpversion.sh
+++ ci_tools/bumpversion.sh
@@ -1,13 +1,5 @@
 bump2version patch --message "Bump version: {current_version} to {new_version} [ci skip]"
 git config --local user.email "engineering@finbourne.com"
 git config --local user.name "finbourne-bot-public"
-git push https://finbourne-bot-public:${GH_TOKEN}@github.com/finbourne/lusid-python-tools.git HEAD:$TRAVIS_BRANCH
-echo "Making commit with incremented version"
-
-
-
-bump2version patch --message "Bump version: {current_version} to {new_version} [ci skip]"
-git config --local user.email "action@github.com"
-git config --local user.name "GitHub Action"
-git push https://slemasne:${gh_token}@github.com/slemasne/repo-test.git HEAD:$TRAVIS_BRANCH
+git push https://finbourne-bot-public:${gh_token}@github.com/finbourne/lusid-python-tools.git HEAD:$TRAVIS_BRANCH
 echo "Making commit with incremented version"
