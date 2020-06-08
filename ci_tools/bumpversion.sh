a6f986aa7e4aa0a689887197579c54773005f725
Stephen LM <20342969+slemasne@users.noreply.github.com>
2020-06-08 17:14:20 +0100

    amend bumpversion to prevent duplicates (#119)
    Co-authored-by: Stephen LeMasney <stephen.lemasney@finbourne.com>

--- ci_tools/bumpversion.sh
+++ ci_tools/bumpversion.sh
@@ -1,5 +1,27 @@
-bump2version patch --message "Bump version: {current_version} to {new_version} [ci skip]"
-git config --local user.email "engineering@finbourne.com"
-git config --local user.name "finbourne-bot-public"
-git push https://finbourne-bot-public:${gh_token}@github.com/finbourne/lusid-python-tools.git HEAD:$TRAVIS_BRANCH
-echo "Making commit with incremented version"
+#!/bin/bash
+
+last_commit_message=$(git log -1 HEAD --pretty=format:%s)
+
+# Bump the version up
+
+if [[ "$last_commit_message" == "Bump version"* ]]; then
+        echo "Skipping version increment (no commits found)"
+else
+
+	# Find next version
+
+	api_version=$(cat __version__.py | grep __version__ |  awk '{split($0, a, "="); print a[2]}' | tr -d ' "')
+	IFS='.' read -a version_split <<< "${api_version}"
+	current_patch=${version_split[2]}
+	new_patch=$(echo "$current_patch + 1" | tr -d $'\r' | bc)
+	new_version="${version_split[0]}.${version_split[1]}.$new_patch"
+    echo "Bumping packaging patch version from $api_version to $new_version"
+
+    # Bump the version in project and make commit back to master
+
+    bump2version patch --message "Bump version in Travis CI: {current_version} to {new_version} [ci skip]"
+	git config --local user.email "engineering@finbourne.com"
+	git config --local user.name "finbourne-bot-public"
+	git push https://finbourne-bot-public:${gh_token}@github.com/finbourne/lusid-python-tools.git HEAD:$TRAVIS_BRANCH
+	echo "Commit with incremented version completed"
+fi
