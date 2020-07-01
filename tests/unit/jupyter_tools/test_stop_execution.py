f17d4d507346b0dd1e30871eaffc05ff57228f2f
Stephen LM <20342969+slemasne@users.noreply.github.com>
2020-07-01 14:01:34 +0100

    add nb (#131)
    * add nb stopper
    
    * Travis making black code formatting
    
    * add stop nb
    
    Co-authored-by: Stephen LM <engineering@finbourne.com>

---
+++ tests/unit/jupyter_tools/test_stop_execution.py
@@ -0,0 +1,14 @@
+import unittest
+from lusidtools.jupyter_tools import StopExecution
+
+
+class TestPortfolioStopper(unittest.TestCase):
+    def test_portfolio_stopper(self) -> None:
+
+        portfolio_code = "testPortfolio"
+        error_message = f"Portfolio {portfolio_code} does not exist. Stopping notebook"
+
+        with self.assertRaises(StopExecution) as error:
+            raise StopExecution(error_message)
+
+        self.assertEqual(error.exception.message, error_message)
