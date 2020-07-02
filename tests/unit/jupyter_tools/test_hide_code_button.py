e81fd6353bc55ae7e76b7bbf3bf0f1031efc4e2b
Stephen LM <20342969+slemasne@users.noreply.github.com>
2020-07-02 19:06:33 +0100

    toggle code (#134)
    Co-authored-by: Stephen LM <engineering@finbourne.com>

--- tests/unit/jupyter_tools/test_hide_code_button.py
+++ tests/unit/jupyter_tools/test_hide_code_button.py
@@ -5,5 +5,5 @@ from lusidtools.jupyter_tools import toggle_code
 class TestHideCodeButton(unittest.TestCase):
     def test_portfolio_stopper(self) -> None:
 
-        test_toggle = toggle_code()
+        test_toggle = toggle_code("Toggle Docstring")
         self.assertIsNone(test_toggle)
