11b6a377d4c4141dc942d28d24eef03ac36e9169
Stephen LM <engineering@finbourne.com>
2020-07-02 17:45:38 +0100

    toggle code

--- lusidtools/jupyter_tools/hide_code_button.py
+++ lusidtools/jupyter_tools/hide_code_button.py
@@ -1,22 +1,23 @@
 from IPython.core.display import display, HTML
 
-toggle_code_str = """
-<form action="javascript:code_toggle()"><input type="submit" id="toggleButton" value="Toggle Docstring"></form>
-"""
 
-toggle_code_prepare_str = """
-     <script>
-     function code_toggle() {
-         if ($('div.cell.code_cell.rendered.selected div.input').css('display')!='none'){
-             $('div.cell.code_cell.rendered.selected div.input').hide();
-         } else {
-             $('div.cell.code_cell.rendered.selected div.input').show();
+def toggle_code(button_message):
+
+    toggle_code_str = f"""
+    <form action="javascript:code_toggle()"><input type="submit" id="toggleButton" value="{button_message}"></form>
+    """
+
+    toggle_code_prepare_str = """
+         <script>
+         function code_toggle() {
+             if ($('div.cell.code_cell.rendered.selected div.input').css('display')!='none'){
+                 $('div.cell.code_cell.rendered.selected div.input').hide();
+             } else {
+                 $('div.cell.code_cell.rendered.selected div.input').show();
+             }
          }
-     }
-     </script>
+         </script>
 
- """
+     """
 
-
-def toggle_code():
     return display(HTML(toggle_code_str + toggle_code_prepare_str))
