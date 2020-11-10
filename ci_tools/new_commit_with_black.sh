fa0138db2e433cb88b63ed4d865026ada296c925
Stephen LM <20342969+slemasne@users.noreply.github.com>
2020-11-10 14:23:36 +0000

    Update new_commit_with_black.sh

--- ci_tools/new_commit_with_black.sh
+++ ci_tools/new_commit_with_black.sh
@@ -1,7 +1,8 @@
 black .
 if [ -n "$(git status --porcelain)" ]; then 
 git commit -m "Travis making black code formatting" -a 
-git push https://\finbourne-bot-public:${GH_TOKEN}@github.com/\finbourne/lusid-python-tools.git HEAD:${GH_BRANCH}
+#git push https://\finbourne-bot-public:${GH_TOKEN}@github.com/\finbourne/lusid-python-tools.git HEAD:${GH_BRANCH}
+git push origin ${GH_BRANCH}
 echo "The black formating tool has made changes. Making commit."
 else 
 echo "No changes detected. Skipping commit"; 
