79fd6cdde459d5a0c331d729e36458700ff82a2a
Stephen LM <20342969+slemasne@users.noreply.github.com>
2020-11-11 09:20:28 +0000

    Enabling black (#165)
    * SE-462: Adding auto black to project
    
    * SE-462: Adding auto black to project
    
    * GitHub Actions making black code formatting
    
    Co-authored-by: Stephen LM <engineering@finbourne.com>

--- ci_tools/new_commit_with_black.sh
+++ ci_tools/new_commit_with_black.sh
@@ -1,8 +1,7 @@
 black .
 if [ -n "$(git status --porcelain)" ]; then 
-git commit -m "Travis making black code formatting" -a 
-#git push https://\finbourne-bot-public:${GH_TOKEN}@github.com/\finbourne/lusid-python-tools.git HEAD:${GH_BRANCH}
-git push origin ${GH_BRANCH}
+git commit -m "GitHub Actions making black code formatting" -a 
+git push origin HEAD:${GH_BRANCH}
 echo "The black formating tool has made changes. Making commit."
 else 
 echo "No changes detected. Skipping commit"; 
