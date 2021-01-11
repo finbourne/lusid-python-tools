d416e3f67f6dbe72c5c030c39d1a093747f5532d
Michael McGarry <mike.mcgarry@finbourne.com>
2021-01-11 08:49:11 +0000

    SE-672 Removed the print statement in the test

--- tests/integration/cocoon/test_cocoon_instruments.py
+++ tests/integration/cocoon/test_cocoon_instruments.py
@@ -649,8 +649,6 @@ class CocoonTestsInstruments(unittest.TestCase):
     )
     def test_load_instrument_lookthrough(self, _, df, mapping):
 
-        print(_)
-
         if "default scope" in _:
             self.skipTest("Default parameter using '$' is not supported")
 
