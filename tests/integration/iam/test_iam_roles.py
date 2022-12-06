75154b41337ce45b077a79bc1f235a34ae75d143
Daniel Freake <daniel.freake@finbourne.com>
2022-12-06 12:39:19 +0000

    CTECH-2361: Reformat Tests

--- tests/integration/iam/test_iam_roles.py
+++ tests/integration/iam/test_iam_roles.py
@@ -16,6 +16,8 @@ from lusidtools import logger
 
 class IAMTestsRoles(unittest.TestCase):
     api_factory = None
+    access_api_factory = None
+    identity_api_factory = None
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -24,10 +26,30 @@ class IAMTestsRoles(unittest.TestCase):
         cls.api_factory = lusid.utilities.ApiClientFactory(
             api_secrets_filename=secrets_file
         )
+
+        # Get the LUSID API URL and the access token from the current api_factory
+        api_client = cls.api_factory.api_client
+        lusid_api_url = api_client.configuration.host
+        access_token = api_client.configuration.access_token
+
+        # Build the access and identity API URLs from the LUSID API URL
+        access_api_url = lusid_api_url[: lusid_api_url.rfind("/") + 1] + "access"
+        identity_api_url = lusid_api_url[: lusid_api_url.rfind("/") + 1] + "identity"
+
+        # Build the access and identity API factories
+        cls.access_api_factory = finbourne_access.utilities.ApiClientFactory(
+            token=access_token,
+            access_url=access_api_url
+        )
+        cls.identity_api_factory = finbourne_identity.utilities.ApiClientFactory(
+            token=access_token,
+            api_url=identity_api_url,
+        )
+
         cls.logger = logger.LusidLogger(os.getenv("FBN_LOG_LEVEL", "info"))
 
     def test_create_role_success(
-        self
+            self
     ) -> None:
         """
         Test that a role can be created successfully
@@ -36,21 +58,11 @@ class IAMTestsRoles(unittest.TestCase):
         """
 
         role_code = "TestRoleCode"
-        access_role_creation_request = access_models.RoleCreationRequest(
-            code=role_code,
-            description="Test Role Description",
-            resource=access_models.RoleResourceRequest(
-                policy_id_role_resource=access_models.PolicyIdRoleResource(
-                    policies=[
-                        access_models.PolicyId(scope="default", code="TestPolicyCode")
-                    ]
-                )
-            ),
-            when=access_models.WhenSpec(
-                activate=datetime.now(tz=pytz.utc) - timedelta(days=2),
-                deactivate=datetime(9999, 12, 31, tzinfo=pytz.utc)
-            )
-        )
+
+        self.delete_role(role_code)
+
+        # Create a role creation request (using the access model)
+        access_role_creation_request = create_access_role_creation_request(role_code)
 
         # Create a role using the LPT create_role method
         iam.roles.create_role(
@@ -58,37 +70,74 @@ class IAMTestsRoles(unittest.TestCase):
             access_role_creation_request
         )
 
-        # Get the LUSID API URL and the access token from the current api_factory
-        api_client = self.api_factory.api_client
-        lusid_api_url = api_client.configuration.host
-        access_token = api_client.configuration.access_token
-
-        # Build the access and identity API URLs from the LUSID API URL.
-        access_api_url = lusid_api_url[: lusid_api_url.rfind("/") + 1] + "access"
-        identity_api_url = lusid_api_url[: lusid_api_url.rfind("/") + 1] + "identity"
-
-        # Build the access and identity API factories.
-        access_api_factory = finbourne_access.utilities.ApiClientFactory(
-            token=access_token,
-            access_url=access_api_url
-        )
-        identity_api_factory = finbourne_identity.utilities.ApiClientFactory(
-            token=api_client.configuration.access_token,
-            api_url=identity_api_url,
-        )
-
-        access_roles_api = access_api_factory.build(finbourne_access.RolesApi)
-        identity_roles_api = identity_api_factory.build(finbourne_identity.RolesApi)
-
-        access_role = access_roles_api.get_role(role_code)
+        # Check that the role was correctly created through the access API
+        access_role = self.get_access_role(role_code)
         self.assertEqual(
             first=access_role.id.code,
             second=role_code
         )
 
-        identity_roles = identity_roles_api.list_roles()
-        identity_roles_codes = [role.role_id.code for role in identity_roles]
-        self.assertIn(
-            member=role_code,
-            container=identity_roles_codes
+        # Check that the role was correctly created through the identity API
+        identity_role = self.get_identity_role(role_code)
+        self.assertEqual(
+            first=identity_role.role_id.code,
+            second=role_code
         )
+
+    def delete_role(
+            self,
+            role_code
+    ):
+        access_roles_api = self.access_api_factory.build(finbourne_access.RolesApi)
+        identity_roles_api = self.identity_api_factory.build(finbourne_identity.RolesApi)
+
+        # Try to delete the role through the access API
+        try:
+            access_roles_api.delete_role(role_code)
+        except Exception as e:
+            print(e)
+
+        identity_role = self.get_identity_role(role_code)
+        try:
+            identity_roles_api.delete_role(identity_role.id)
+        except Exception as e:
+            print(e)
+
+    def get_access_role(
+            self,
+            role_code
+    ):
+        access_roles_api = self.access_api_factory.build(finbourne_access.RolesApi)
+
+        return access_roles_api.get_role(role_code)
+
+    def get_identity_role(
+            self,
+            role_code
+    ):
+        # Build the identity API factory
+        identity_roles_api = self.identity_api_factory.build(finbourne_identity.RolesApi)
+
+        roles = identity_roles_api.list_roles()
+
+        return next((r for r in roles if r.role_id.code == role_code))
+
+
+def create_access_role_creation_request(
+        role_code
+):
+    return access_models.RoleCreationRequest(
+        code=role_code,
+        description="Test Role Description",
+        resource=access_models.RoleResourceRequest(
+            policy_id_role_resource=access_models.PolicyIdRoleResource(
+                policies=[
+                    access_models.PolicyId(scope="default", code="TestPolicyCode")
+                ]
+            )
+        ),
+        when=access_models.WhenSpec(
+            activate=datetime.now(tz=pytz.utc) - timedelta(days=2),
+            deactivate=datetime(9999, 12, 31, tzinfo=pytz.utc)
+        )
+    )
