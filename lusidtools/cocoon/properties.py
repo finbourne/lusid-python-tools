95281a1ed3a3c71d221301ddf8a0696cbcdeba7c
Adam Massoud <adam.massoud@finbourne.com>
2023-01-11 20:42:15 +0000

    SE-612: Remove unique() calls as set() does this. remove uneccessary conversion of set to list (both are iterables)

--- lusidtools/cocoon/properties.py
+++ lusidtools/cocoon/properties.py
@@ -180,7 +180,7 @@ def create_property_definitions_from_file(
 
     # Ensure that all data types in the file have been mapped
     actual_data_types = set(
-        [str(data_type) for data_type in missing_property_data_frame.dtypes.unique()]
+        [str(data_type) for data_type in missing_property_data_frame.dtypes]
     )
     allowed_data_types = set(global_constants["data_type_mapping"])
     if not (actual_data_types <= allowed_data_types):
@@ -344,12 +344,12 @@ def create_property_values(
     properties : dict {str, models.PerpetualProperty}
     """
 
-    actual_data_types = set([str(data_type) for data_type in dtypes.unique()])
+    actual_data_types = set([str(data_type) for data_type in dtypes])
     allowed_data_types = set(global_constants["data_type_mapping"])
 
     # Ensure that all data types in the file have been mapped
     if not (actual_data_types <= allowed_data_types):
-        unmapped_data_types = list(actual_data_types - allowed_data_types)
+        unmapped_data_types = actual_data_types - allowed_data_types
         unmapped_columns = dtypes[dtypes.isin(unmapped_data_types)]
         raise TypeError(
             invalid_columns_error_message(unmapped_columns, allowed_data_types)
