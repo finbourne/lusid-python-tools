90e72ada3f0b8a655ffd26701d32e691003596e7
Adam Massoud <adam.massoud@finbourne.com>
2023-04-17 11:38:59 +0100

    CTECH-2907: reset bumpversion to use correct creds

--- ci_tools/bumpversion.sh
+++ ci_tools/bumpversion.sh
@@ -11,6 +11,6 @@ else
 	poetry version patch
 	git add pyproject.toml
 	git commit -m "Bump version to $(poetry version -s)"
-	git push -f https://${GH_TOKEN}@github.com/adamMQM/lusid-python-tools.git HEAD:master
+	git push -f https://finbourne-bot-public:${GH_TOKEN}@github.com/finbourne/lusid-python-tools.git HEAD:master
 	echo "Commit with incremented version completed"
 fi
