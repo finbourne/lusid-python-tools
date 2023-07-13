87d8d68bbf8c5faf1a70e923e8c0ad848a95bd53
Adam Massoud <adam.massoud@finbourne.com>
2023-07-13 11:10:36 +0100

    CTECH-3241: Use pandas.testing instead of pandas.utils.testing

--- tests/unit/cocoon/test_utilities.py
+++ tests/unit/cocoon/test_utilities.py
@@ -11,7 +11,7 @@ from typing import Callable
 import lusid
 import numpy as np
 import pandas as pd
-from pandas.util.testing import assert_frame_equal, assert_series_equal
+from pandas.testing import assert_frame_equal, assert_series_equal
 import pytz
 from parameterized import parameterized
 from lusidtools import cocoon
