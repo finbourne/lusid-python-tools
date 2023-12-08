ad12b07d16785a917db52bb43dbf98ef82a9c002
Adam Massoud <adam.massoud@finbourne.com>
2023-12-08 15:05:51 +0000

    CTECH-3303: remove hide code button

--- tests/unit/jupyter_tools/test_hide_code_button.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from lusidtools.jupyter_tools import toggle_code
-
-
-class TestHideCodeButton(unittest.TestCase):
-    def test_portfolio_stopper(self) -> None:
-        test_toggle = toggle_code("Toggle Docstring")
-        self.assertIsNone(test_toggle)
